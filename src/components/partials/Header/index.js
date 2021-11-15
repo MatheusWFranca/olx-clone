d010026057a56432f56777b5d6af313fd3e8991b
MatheusWFranca <mateusanches2@gmail.com>
2021-11-15 20:54:32 -0300

    Header #1

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom'
 import { HeaderArea } from './styled'
 
 function Header() {
-  return <HeaderArea></HeaderArea>
+  return (
+    <HeaderArea>
+      <div className="container">
+        <div className="logo">
+          <Link to="/home">
+            <span>OLX</span>
+          </Link>
+        </div>
+      </div>
+    </HeaderArea>
+  )
 }
 
 export default Header
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,6 +1,28 @@
 import styled from 'styled-components'
 
 export const HeaderArea = styled.header`
-  height: 100px;
+  height: 6rem;
   background-color: #1c1c1c;
+  border-bottom: 1px solid #ccc;
+
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    display: flex;
+    background: #fff;
+    align-items: center;
+  }
+
+  .logo {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    height: 6rem;
+
+    span {
+      font-size: 2.7rem;
+      font-weight: bold;
+      color: #e2b;
+    }
+  }
 `
