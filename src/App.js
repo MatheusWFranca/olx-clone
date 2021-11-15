9902f9aa9a472f525cbf9b95934f6848ca617b85
MatheusWFranca <mateusanches2@gmail.com>
2021-11-15 20:08:28 -0300

    Global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import './App.css'
 import { connect } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { Template } from './components/MainComponents'
-import Routes from './Routes'
 
+import { Template } from './components/MainComponents'
 import Header from './components/partials/Header'
 import Footer from './components/partials/Footer'
+
+import Routes from './Routes'
 
 function Page(props) {
   return (
