0ba18f26d61d45e29047885d09993a61be96b904
MatheusWFranca <mateusanches2@gmail.com>
2021-11-15 22:12:38 -0300

    AuthHandler

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
 import { HeaderArea } from './styled'
 
+import { isLogged } from '../../../helpers/AuthHandler'
+
 function Header() {
+  let logged = isLogged()
+
   return (
     <HeaderArea>
       <div className="container">
@@ -12,14 +16,27 @@ function Header() {
         </div>
         <nav>
           <ul>
+            {logged ? (
+              <>
+                <li>
+                  <Link to="/my-account">Minha Conta</Link>
+                </li>
+                <li>
+                  <Link to="/logout">Sair</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/signin">Entrar</Link>
+                </li>
+                <li>
+                  <Link to="/signup">Cadastrar</Link>
+                </li>
+              </>
+            )}
             <li>
-              <Link to="/">Login</Link>
-            </li>
-            <li>
-              <Link to="/">Cadastrar</Link>
-            </li>
-            <li>
-              <Link className="button" to="/">
+              <Link className="button" to="/post-an-ad">
                 Anunciar
               </Link>
             </li>
